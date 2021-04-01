7c7fd833d25ea31e17bb7cb9dc36a1ba7d42aa9b
gabriel pontes <gabrielmnp95@yahoo.com.br>
2021-04-01 19:13:38 -0300

    adicionei um programa para solucionar o problema do barbeiro dorminhoco

---
+++ barbeiro.cpp
@@ -0,0 +1,91 @@
+#include<mutex>
+#include<condition_variable>
+#include<iostream>
+#include<thread>
+#include<vector>
+using namespace std;
+
+int cont=0; //contador para as pessos que cortaram o cabelo
+int caideiras=50;//maximo de pessoas que cabe na barbearia
+int quantidade=100000;//nao posso criar milhares de threads entao cada thread vai executar esse tanto de chamadas
+int cont1=0;//contador pra quantas pessoas o barbeiro cortou o cabelo
+class mobarbeiro {
+    private:
+        int maxPessoas;//maximo de pessoas que cabe na barbearia         
+        int numPessoas=0;//numero de pessoas dentro da barbearia
+        int cortando=-1;//guarda o id do cliente que ta cortando o cabelo
+        int chamadas=0;//pra saber quando parar
+        condition_variable esperando,barbeiroespera;
+        mutex mux;
+    public:
+        mobarbeiro (int cadeiras){
+            maxPessoas=cadeiras;
+        }
+        void cliente(int id){
+            unique_lock<mutex> lck(mux);
+            if (numPessoas==0){//nao tem ninguem esperando
+                numPessoas++;
+                if(cortando!=-1)
+                    cout << "erro cliente \n";
+                cortando=id;
+                cont++;
+                barbeiroespera.notify_one();
+            }
+            else if (numPessoas<maxPessoas){//espera pra cortar
+                numPessoas++;
+                while(cortando!=-1)
+                    esperando.wait(lck);
+                if(cortando!=-1)
+                    cout << "erro cliente \n";
+                cortando=id;
+                cont++;
+                barbeiroespera.notify_one();
+            }
+            else{//vai embora
+                chamadas++;
+                return;
+            }
+            chamadas++;
+            }
+        void barbeiro(){
+            while(true){
+                unique_lock<mutex> lck(mux);
+                while (numPessoas==0)//caso nao tenha ninguem dorme
+                    barbeiroespera.wait(lck);
+                if(cortando==-1)
+                    cout << "erro barbeiro \n";
+                //corta o cabelo
+                cortando=-1;
+                cont1++;
+                numPessoas--;
+                esperando.notify_one();
+                if(chamadas==(quantidade*100))
+                    return;
+                barbeiroespera.wait(lck);//espera o cara sentar pra cortar o cabelo dele
+            }
+        }
+};
+mobarbeiro objeto(caideiras);
+void barbeiro(){
+    objeto.barbeiro();
+}
+void cliente(int id){
+    for(int i=0;i<quantidade;i++)
+    objeto.cliente((id*quantidade+i));
+}
+int main(){
+    vector<thread> threads;
+    thread b(barbeiro);
+    threads.push_back(move(b));
+    for(int i=0;i<100;i++){
+        thread t(cliente,i);
+        threads.push_back(move(t));
+    }
+
+    for ( thread &t : threads){
+        t.join();
+    }
+    cout << cont << " " << cont1 << endl;//para ver se o numero de pessoas que cotaram o cabelo é igual ao numero de pessoas que o barbeiro cortou o cabelo
+
+    return 0;
+}
--- pratica3.cpp
+++ pratica3.cpp
@@ -2,102 +2,117 @@
 #include<condition_variable>
 #include<iostream>
 #include<thread>
-#include <unistd.h>
-#include <vector>
+#include<vector>
 using namespace std;
 
-vector <thread> threads;
+int cont=0; //contador usado fora do monitor apenas para teste
 
-class MonitorCaixaPostal {
-private:
-    int *carro;
-    condition_variable cheio, vazio,pentrar;
-    mutex mux;
-    int n,c;
-    int dentro=0;
-    int saiu=0;
-public:
-
-MonitorCaixaPostal(int a,int b){
-    n=a;
-    c=b;
-    carro=new int[c];
-    for (int i = 0; i < c; i++){
-        carro[i]=-1;
-    }
-    
-}
-~MonitorCaixaPostal(){
-    delete [] carro;
-}
-void espera_encher(){
-    unique_lock<mutex> lck(mux);
-    while (dentro!=c){
-        vazio.wait(lck);
-    }
-}
-void espera_esvaziar(){
-    unique_lock<mutex> lck(mux);
-    while (saiu!=0){
-        cheio.wait(lck);
-    }
-    pentrar.notify_all();
-}
-void entra_no_carro(int id){
-    unique_lock<mutex> lck(mux);
-    while (dentro==c){
-        pentrar.wait(lck);
-    }
-    for (int i = 0; i < c; i++){
-        if (carro[i]==-1){
-            carro[i]=id;
-            break;
+class MonitorMontanhaRussa {
+    private:
+        int maxPessoas;         //capacidade máxima do carro
+        int numPessoas=0;       //pessoas dentro do carro
+        bool andando=false;     //carro dando volta
+        bool esvaziando=false;  //passageiros estão saindo do carro
+        condition_variable podeentrar,podesair,podeandar, vazio;
+        mutex mux;
+    public:
+        MonitorMontanhaRussa(int capacidade) {
+            maxPessoas=capacidade;
         }
-    }
-    dentro++;
-    saiu--;
-    if (dentro==c)
-        vazio.notify_one();
-}
-void sai_do_carro(int id){
-    
-}
-void teste(){
-    for (int i = 0; i < c; i++){
-        cout<<carro[i]<<" ";
-    }
-    cout <<endl;
-}
 
+        ~MonitorMontanhaRussa() {}
+
+        void espera_encher() {
+            unique_lock<mutex> lck(mux);
+            podeentrar.notify_all();
+            //cout<<"espera encher"<<endl;
+            podeandar.wait(lck, [this]{return numPessoas==maxPessoas;});
+            andando=true;
+        }
+
+        void espera_esvaziar() {
+            unique_lock<mutex> lck(mux);
+            andando=false;
+            esvaziando=true;    //evitar que alguém entre antes que todos saiam
+            podesair.notify_all();
+            //cout<<"espera esvaziar"<<endl;
+            vazio.wait(lck,[this]{ return numPessoas==0;} );
+            esvaziando=false;
+        }
+
+        void entra_no_carro(int id, int i) {
+            unique_lock<mutex> lck(mux);
+            podeentrar.wait(lck, [this]{ return !andando && !esvaziando && numPessoas<maxPessoas;});
+            numPessoas++;
+            cont++;
+            //cout<<"cliente "<<id<<" entra para volta "<<i<<endl;
+            if(numPessoas==maxPessoas)
+                podeandar.notify_one();
+        }
+
+        void sai_do_carro(int id, int i){
+            unique_lock<mutex> lck(mux);
+            podesair.wait(lck, [this]{return !andando && esvaziando;});
+            numPessoas--;
+            cont--;
+            //cout<<"cliente "<<id<<" sai"<<endl;
+            if(numPessoas==0)
+                vazio.notify_one();
+        }
 };
 
-void montanha(int n,MonitorCaixaPostal &russa){
+int capacidade=20;
+
+/* Testa se a quantidade de pessoas é diferente do esperado */
+void dandoVolta() {
+    if(cont!=capacidade)
+        cout<<"cont = "<<cont<<endl;
+}
+
+pthread_barrier_t *bar;
+MonitorMontanhaRussa montanharussa(capacidade);
+
+void carro(int n) {
     for(int i=0; i<n; ++i) {
-        russa.espera_encher();
-        cout << "saiu: ";
-        russa.teste();
-        russa.espera_esvaziar();
+        if (i%5000==0) cout<<"preparando volta "<<i<<endl;
+        montanharussa.espera_encher();
+        dandoVolta();   //verifica se cont tem valor inválido
+        /*Dar chance para que outras threads executem. Se tudo estiver 
+        funcionando isso não fará diferença porque o carro está andando e
+        ninguém pode sair e nem entrar */
+        this_thread::yield(); 
+        dandoVolta();   //verifica novamente se cont foi alterado
+        montanharussa.espera_esvaziar();
     }
 }
 
-void cliente(int n,int id,MonitorCaixaPostal &russa) {
-    for(int i=0;i<n;i++)
-        russa.entra_no_carro(id);
-}
-int main(){
-    int c;
-    cin >>c;
-    int n=10*c;
-    int v;
-    cin>>v;
-    MonitorCaixaPostal russa(n,c);
-    thread t1(montanha,10*v,ref(russa));
-    for (int j = 0; j < n; j++){
-        thread t(cliente,v,j,ref(russa));
-        threads.push_back(move(t));
+void cliente(int id, int n) {
+    for(int i=0; i<n; ++i) {
+        montanharussa.entra_no_carro(id,i);
+        montanharussa.sai_do_carro(id,i);
+
+        /* barreira utilizada para fazer com que todos os clientes do parque 
+        deem a volta i antes de começar a volta i+1 */
+        pthread_barrier_wait(&bar[i]);
     }
-    for(auto &t:threads)
+}
+
+int main() {
+    int ncli=capacidade*2;
+    int voltasCarro = 100000;
+    int voltasCli = voltasCarro/2;
+
+    bar=new pthread_barrier_t[voltasCli];
+    for (int i=0; i<voltasCli; ++i)
+        pthread_barrier_init(&bar[i],NULL,ncli);
+
+    vector<thread> threads;
+    threads.push_back(thread(carro,voltasCarro));
+    for(int i=1; i<=ncli; ++i)
+        threads.push_back(thread(cliente,i,voltasCli));
+    
+    for(thread &t : threads)
         t.join();
-    t1.join();
-    return 0;
+
+    delete [] bar;
 }
